79ffd154ab43a4ca66666817cdaf48c0801601f4
randicc <2183514660@qq.com>
2026-07-29 12:27:21 +0800

    yes

---
+++ next.config.cjs
@@ -0,0 +1,19 @@
+const withPWA = require("@ducanh2912/next-pwa").default
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  turbopack: {},
+}
+
+module.exports = withPWA({
+  dest: "public",
+  disable: false,
+  register: true,
+  skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndCaching: true,
+  staticPageGenerationTimeout: 180,
+})(nextConfig)
